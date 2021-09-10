55c29da5724ed398bfa3e26e55f44b9848d20bd0
rath17023 <39741642+rath17023@users.noreply.github.com>
2021-09-09 20:42:36 -0500

    Update Dockerstart.sh

--- Dockerstart.sh
+++ Dockerstart.sh
@@ -1,6 +1,32 @@
+#Step 1
+#Create your start script /Dockerstart.sh
+#Code I use below
+
+#Step 2
+#Create the following service file with Sudo
+#	sudo nano /etc/systemd/system/npm-autostart.service
+#		[Unit]
+#		After=network.service
+#
+#		[Service]
+#		ExecStart=# Path to Docker Compose /Dockerstart.sh
+#
+#		[Install]
+#		WantedBy=default.target
+
+#Step 3
+#Modify the permissions of both files you just created with the following commands.
+#	sudo chmod 744 # Path to Docker Compose /Dockerstart.sh
+#	sudo chmod 664 /etc/systemd/system/npm-autostart.service
+
+#Step 4
+#Reload the daemon and enable the service
+#	sudo systemctl daemon-reload
+#	sudo systemctl enable npm-autostart.service
+
 #!/bin/sh
 ## Starting docker containers
 ## Often I set this up with an init script to auto star/run. I am sure there are better ways but this was the way I know of that works.
 
-date
+cd #Path to where docker Compose is installed this shouldn't be needed but seems to make things more stable.
 docker-compose up -d
